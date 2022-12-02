7defebcd42fbe88458737d522f7e4c009d1dfb76
eshaansabur <eshaansabur12@gmail.com>
2022-12-02 19:04:30 +0600

    protected

--- src/components/Firebase/firebase.init.js
+++ src/components/Firebase/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCBN_dG2je-Nb81B3YyS7p2m-088tcIFxE",
-  authDomain: "node-jwt-recap.firebaseapp.com",
-  projectId: "node-jwt-recap",
-  storageBucket: "node-jwt-recap.appspot.com",
-  messagingSenderId: "651410249459",
-  appId: "1:651410249459:web:c8372ca2d84af71880c986"
+  apiKey:process.env.REACT_APP_apiKey,
+  authDomain:process.env.REACT_APP_authDomain,
+  projectId:process.env.REACT_APP_projectId,
+  storageBucket:process.env.REACT_APP_storageBucket,
+  messagingSenderId:process.env.REACT_APP_messagingSenderId,
+  appId:process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
